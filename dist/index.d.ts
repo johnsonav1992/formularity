3516b4717b3abf24cb55ea0bc3b04dffc1030545
Alex Johnson <97169070+johnsonav1992@users.noreply.github.com>
2024-01-24 21:42:54 -0600

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export * from './types/types';
-type TestComponentProps = {
-    textText?: string;
-};
-export declare const TestComponent: React.FC<TestComponentProps>;
