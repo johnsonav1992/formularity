ccc092b2abcaa89ceb96b61017a7dc2c016d8cb4
Alex Johnson <johnsonav1992@gmail.com>
2024-01-25 05:23:34 -0600

    private

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,19 @@
-const postcss = require('rollup-plugin-postcss');
-const typescript = require('@rollup/plugin-typescript');
-const peerDepsExternal = require('rollup-plugin-peer-deps-external');
-const resolve = require('@rollup/plugin-node-resolve').default;
-const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require( '@rollup/plugin-typescript' );
+const peerDepsExternal = require( 'rollup-plugin-peer-deps-external' );
+const resolve = require( '@rollup/plugin-node-resolve' ).default;
+const commonjs = require( '@rollup/plugin-commonjs' );
 
 module.exports = {
-  input: 'src/index.tsx',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    sourcemap: true,
-  },
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    typescript(),
-    commonjs(),
-    postcss({
-      modules: true,
-    }),
-  ],
+    input: 'src/index.tsx'
+    , output: {
+        dir: 'dist'
+        , format: 'cjs'
+        , sourcemap: true
+    }
+    , plugins: [
+        peerDepsExternal()
+        , resolve()
+        , typescript()
+        , commonjs()
+    ]
 };
