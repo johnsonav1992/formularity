15876ebbce9bf0209c51e23728230883002a7ebf
Alex Johnson <johnsonav1992@gmail.com>
2024-02-06 12:55:46 -0600

    type fleshing out

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -2,14 +2,14 @@ export type Prettify<T> = {
     [K in keyof T]: T[K];
 } & {};
 
-type Keys<O, IsTop, K extends string | number> =
-    IsTop extends true
-        ? K | ( O extends unknown[] ? `[${ K }]` : never )
-        : `.${ K }` | ( O extends unknown[] ? `[${ K }]` | `.[${ K }]` : never );
+type Keys<TObj, IsRoot, TKey extends string | number> =
+    IsRoot extends true
+        ? TKey | ( TObj extends unknown[] ? `[${ TKey }]` : never )
+        : `.${ TKey }` | ( TObj extends unknown[] ? `[${ TKey }]` | `.[${ TKey }]` : never );
 
-export type DeepKeys<T, IsTop = true, K extends keyof T = keyof T> =
-    K extends string | number
-        ? `${ Keys<T, IsTop, K> }${ '' | ( T[K] extends object ? DeepKeys<T[K], false> : '' ) }`
+export type DeepKeys<TObj, IsRoot = true, TKey extends keyof TObj = keyof TObj> =
+    TKey extends string | number
+        ? `${ Keys<TObj, IsRoot, TKey> }${ '' | ( TObj[TKey] extends object ? DeepKeys<TObj[TKey], false> : '' ) }`
         : never;
 
 export type DeepValue<TObj, TKey> = TObj extends Record<PropertyKey, unknown>
