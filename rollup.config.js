7e195da756035fcda38c6bc2b0aede04f177fa8f
Alex Johnson <johnsonav1992@gmail.com>
2024-03-07 05:31:12 -0600

    add terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,20 @@ const typescript = require( '@rollup/plugin-typescript' );
 const peerDepsExternal = require( 'rollup-plugin-peer-deps-external' );
 const resolve = require( '@rollup/plugin-node-resolve' ).default;
 const commonjs = require( '@rollup/plugin-commonjs' );
+const terser = require( '@rollup/plugin-terser' );
 
 module.exports = {
     input: 'src/index.ts'
     , output: {
         dir: 'dist'
         , format: 'esm'
-        , sourcemap: false
+        , sourcemap: true
     }
     , plugins: [
         peerDepsExternal()
         , resolve()
         , typescript()
         , commonjs()
+        , terser()
     ]
 };
