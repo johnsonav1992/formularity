7bb377ea3ee894fff36b69bd886d2604e9f0ccf4
Alex Johnson <johnsonav1992@gmail.com>
2024-06-10 05:46:04 -0500

    vitest added

---
+++ tests/sample.test.ts
@@ -0,0 +1,11 @@
+import {
+    it
+    , expect
+    , describe
+} from 'vitest';
+
+describe( 'Vitest works!', () => {
+    it( 'should run a test properly', () => {
+        expect( 1 ).toBeTruthy();
+    } );
+} );
