12846bd1c8ae04c49daf53f38a1d78321927047f
Alex Johnson <johnsonav1992@gmail.com>
2024-06-10 07:18:46 -0500

    trying to get this spy to work

--- tests/components/ResetButton.test.tsx
+++ tests/components/ResetButton.test.tsx
@@ -3,11 +3,13 @@ import {
     , expect
     , describe
     , afterEach
+    , vi
 } from 'vitest';
 import {
     render
     , screen
     , cleanup
+    , fireEvent
 } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import { ResetButton } from '../../src/ResetButton';
@@ -41,5 +43,31 @@ describe( 'ResetButton', () => {
         const button = screen.getByRole( 'button' );
 
         expect( button ).toHaveTextContent( text );
+    } );
+
+    it( 'should fire a reset event on a form if clicked', () => {
+        const handleReset = () => {};
+
+        render(
+            <>
+                <form
+                    name='form'
+                    onReset={ handleReset }
+                >
+                    <ResetButton>
+                        Reset
+                    </ResetButton>
+                </form>
+            </>
+        );
+
+        const button = screen.getByRole( 'button' );
+        const form = screen.getByRole( 'form' );
+        const handleResetSpy = vi.spyOn( handleReset, 'handleReset' as never );
+
+        button.click();
+        fireEvent.reset( form );
+
+        expect( handleResetSpy ).toHaveBeenCalledOnce();
     } );
 } );
