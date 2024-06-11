64c5eb3848257a179dbab5099edf657a286033e5
Alex Johnson <johnsonav1992@gmail.com>
2024-06-11 06:34:34 -0500

    tests coming along!

--- tests/components/ResetButton.test.tsx
+++ tests/components/ResetButton.test.tsx
@@ -3,71 +3,80 @@ import {
     , expect
     , describe
     , afterEach
-    , vi
+    , beforeAll
 } from 'vitest';
 import {
     render
     , screen
-    , cleanup
-    , fireEvent
 } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+import userEvent from '@testing-library/user-event';
 import { ResetButton } from '../../src/ResetButton';
+import {
+    Formularity
+    , createFormStore
+} from '../../src';
+
+const text = 'Reset';
+const initialValues = {
+    firstName: ''
+    , lastName: ''
+    , email: ''
+};
+
+beforeAll( () => {
+    const formStore = createFormStore( { initialValues } );
+
+    render(
+        <>
+            <Formularity formStore={ formStore }>
+                { ( { Field } ) => (
+                    <>
+                        <Field
+                            name='firstName'
+                            label='First Name'
+                        />
+                        <Field name='lastName' />
+                        <Field name='email' />
+                        <ResetButton>
+                            { text }
+                        </ResetButton>
+                    </>
+                ) }
+            </Formularity>
+        </>
+    );
+} );
 
 afterEach( () => {
-    cleanup();
+    // cleanup();
 } );
 
 describe( 'ResetButton', () => {
-    it( 'should render a ResetButton', () => {
-        render( <ResetButton /> );
-    } );
+    it( 'should render a ResetButton', () => {} );
 
     it( 'should have a type of "reset" ', () => {
-        render( <ResetButton /> );
-
         const button = screen.getByRole( 'button' );
 
         expect( button ).toHaveAttribute( 'type', 'reset' );
     } );
 
     it( 'should render a ResetButton with some text inside', () => {
-        const text = 'Reset';
-
-        render(
-            <ResetButton>
-                { text }
-            </ResetButton>
-        );
-
         const button = screen.getByRole( 'button' );
 
         expect( button ).toHaveTextContent( text );
     } );
 
-    it( 'should fire a reset event on a form if clicked', () => {
-        const handleReset = () => {};
+    it( 'should reset the form if it has been altered', async () => {
+        const user = userEvent.setup();
+        const fields = screen.getAllByRole( 'textbox' );
+        const firstNameField = fields[ 0 ];
+        const resetButton = screen.getByRole( 'button' );
 
-        render(
-            <>
-                <form
-                    name='form'
-                    onReset={ handleReset }
-                >
-                    <ResetButton>
-                        Reset
-                    </ResetButton>
-                </form>
-            </>
-        );
+        await user.type( firstNameField, 'Alex J.' );
+        expect( firstNameField ).toHaveValue( 'Alex J.' );
 
-        const button = screen.getByRole( 'button' );
-        const form = screen.getByRole( 'form' );
-        const handleResetSpy = vi.spyOn( handleReset, 'handleReset' as never );
-
-        button.click();
-        fireEvent.reset( form );
-
-        expect( handleResetSpy ).toHaveBeenCalledOnce();
+        await user.click( resetButton );
+        expect( firstNameField ).toHaveValue( initialValues.firstName );
     } );
 } );
