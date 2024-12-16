132e8d9466c5d99a2ce392eaa6901b4cf585e45d
Alex Johnson <johnsonav1992@gmail.com>
2024-12-15 22:04:27 -0600

    add docs

--- src/component-library-configs/mui.ts
+++ src/component-library-configs/mui.ts
@@ -2,6 +2,12 @@ import { FC } from 'react';
 import { ComponentLibraryConfig } from './types';
 import { Checkbox } from '@mui/material';
 
+/**
+ * Configuration plugin for Material UI.
+ * Allows all MUI components to work flawlessly
+ * with Formularity without any additional setup.
+ * @returns {ComponentLibraryConfig}
+ */
 export function mui (): ComponentLibraryConfig {
     return {
         libraryName: 'mui'
